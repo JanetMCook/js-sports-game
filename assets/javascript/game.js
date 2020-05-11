25b62cdba09073bf6934195f6c661e87ecb6106a
Janet Cook <janetmariecook@gmail.com>
2020-05-11 06:58:40 -0600

    first submit

---
+++ assets/javascript/game.js
@@ -0,0 +1,21 @@
+let teamoneShootButton = document.querySelector('#teamone-shoot-button')
+let teamtwoShootButton = document.querySelector('#teamtwo-shoot-button')
+let resetButton = document.querySelector('#reset-button')
+
+
+teamoneShootButton.addEventListener('click'),function (event) {
+    alert('button clicked')
+
+// let teamoneNumshots = parseInt(teamone-numshots.innerText) + 1
+    // teamone-numbshots.innerText = teamoneNumshots
+    
+// teamtwoShootButton.addEventListener('click'),function () {
+    // let teamtwoNumshots = parseInt(teamtwo-numbshots.innerText) + 1
+    // teamtwo-numshots.innerText = teamtwoShootButton
+       
+    // let randNumber = math.random
+        // if (math.random() > 0.5) {
+        // console.log("Swish!")
+        // } else{
+        // console.log("Airhorn!")
+}
