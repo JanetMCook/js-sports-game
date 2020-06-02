e63494b8c5edc96042fb27dbc90b6cc3c5cf248e
Janet Cook <janetmariecook@gmail.com>
2020-06-02 07:13:11 -0600

    Made changes to assessment to complete

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,21 +1,55 @@
+let resetButton = document.querySelector('#reset-button')
+let resetCounterElement = document.querySelector('#num-resets')
 let teamoneShootButton = document.querySelector('#teamone-shoot-button')
 let teamtwoShootButton = document.querySelector('#teamtwo-shoot-button')
-let resetButton = document.querySelector('#reset-button')
+let teamoneNumshotsElement = document.querySelector('#teamone-numshots')
+let teamtwoNumshotsElement = document.querySelector('#teamtwo-numshots')
+let teamoneGoalsElement = document.querySelector('#teamone-numgoals')
+let teamtwoGoalsElement = document.querySelector('#teamtwo-numgoals')
 
+resetButton.addEventListener('click', function () {
+    console.log('reset clicked')
+    let numResets = Number(resetCounterElement.innerHTML) + 1
+    resetCounterElement.innerHTML = numResets
 
-teamoneShootButton.addEventListener('click'),function (event) {
-    alert('button clicked')
+    teamoneNumshotsElement.innerHTML = 0
+    teamtwoNumshotsElement.innerHTML = 0
+    teamoneGoalsElement.innerHTML = 0
+    teamtwoGoalsElement.innerHTML = 0
 
-// let teamoneNumshots = parseInt(teamone-numshots.innerText) + 1
-    // teamone-numbshots.innerText = teamoneNumshots
+})
+
+teamoneShootButton.addEventListener('click', function () {
+    console.log('Team 1 button')
+
+    let teamoneNumshots = parseInt(teamoneNumshotsElement.innerHTML) + 1
+    teamoneNumshotsElement.innerHTML = teamoneNumshots
     
-// teamtwoShootButton.addEventListener('click'),function () {
-    // let teamtwoNumshots = parseInt(teamtwo-numbshots.innerText) + 1
-    // teamtwo-numshots.innerText = teamtwoShootButton
-       
-    // let randNumber = math.random
-        // if (math.random() > 0.5) {
-        // console.log("Swish!")
-        // } else{
-        // console.log("Airhorn!")
-}
+    let randNumber = Math.floor(Math.random() * 3) + 1
+    if (Math.random() > .5) {
+        let teamoneNumGoals = parseInt(teamoneGoalsElement.innerHTML) + 1
+        teamoneGoalsElement.innerHTML = teamoneNumGoals
+        console.log("Swish!")
+    } else {
+        (Math.random() < .5)
+        console.log("Airhorn!")
+    }
+})
+
+teamtwoShootButton.addEventListener('click', function () {
+    console.log('Team 2 button')
+
+    let teamtwoNumshots = parseInt(teamtwoNumshotsElement.innerHTML) + 1
+    teamtwoNumshotsElement.innerHTML = teamtwoNumshots
+
+    
+    let randNumber = Math.floor(Math.random() * 3) + 1
+    if (Math.random() > .5) {
+        let teamtwoGoals = parseInt(teamtwoGoalsElement.innerHTML) + 1
+        teamtwoGoalsElement.innerHTML = teamtwoGoals
+        console.log("Swish!")
+    } else {
+        (Math.random() < .5)
+        console.log("Airhorn!")
+    }
+})
